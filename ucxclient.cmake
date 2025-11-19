e62dba4bd4bd7b1b80626e5d06a8d7b1ac9819d6
Andreas Anderberg <andreas.anderberg@u-blox.com>
2025-11-19 14:11:53 +0100

    Move generated c code to separate dir

--- ucxclient.cmake
+++ ucxclient.cmake
@@ -1,5 +1,11 @@
 set(REPO_DIR ${CMAKE_CURRENT_LIST_DIR})
-file(GLOB UCXCLIENT_UCX_API_SRC ${REPO_DIR}/ucx_api/*.c)
+file(GLOB UCXCLIENT_UCX_API_SRC
+    ${REPO_DIR}/ucx_api/*.c
+    ${REPO_DIR}/ucx_api/generated/*.c
+)
 file(GLOB UCXCLIENT_AT_API_SRC ${REPO_DIR}/src/*.c)
-set(UCXCLIENT_INC ${REPO_DIR}/inc ${REPO_DIR}/ucx_api)
+set(UCXCLIENT_INC ${REPO_DIR}/inc
+    ${REPO_DIR}/ucx_api
+    ${REPO_DIR}/ucx_api/generated
+)
 set(UCXCLIENT_PORT_DIR ${REPO_DIR}/examples/port)
